ec1a946d899a50d9a295ea7ea4aefed20c47f6f3
mosheeliyahu <moshe.eliyahu@mail.huji.ac.il>
2016-08-10 02:10:59 +0300

    init

---
+++ Assets/eat.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eat : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<AudioSource>().Play();
+        Destroy(this.gameObject);
+        other.GetComponent<steering>().speed += 2;
+    }
+}
